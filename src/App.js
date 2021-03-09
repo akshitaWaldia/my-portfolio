4fd10db8d8deab45a4f62368514cb9114937f3c5
Akshita <56341374+akshita707@users.noreply.github.com>
2021-03-09 20:01:29 +0530

    first-commit
    Working on Home Page: sidebar

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.scss';
+import NavBar from './Components/NavBar.js';
+function App() {
+  return (
+    <div className="App">
+     
+        <div className="sidebar">
+            <NavBar />
+        </div>
+        <div className="main-content"></div>
+        
+    
+    </div>
+  );
+}
+
+export default App;
